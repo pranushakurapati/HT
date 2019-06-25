670563d9c9b01f33625885dd2ff4d6bb7714e4bd
saggezza-cloud-analytics <51955859+saggezza-cloud-analytics@users.noreply.github.com>
2019-06-25 16:14:39 -0500

    python Coding standards
    Implemented pep8 and pyflakes recommendations

--- Python_scripts/common_utils/stg_common_utils.py
+++ Python_scripts/common_utils/stg_common_utils.py
@@ -5,28 +5,27 @@ import csv
 
 from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-#from fsplit.filesplit import FileSplit
+# from fsplit.filesplit import FileSplit
 
 
 def create_connection(engine_name):
-    engine = create_engine(URL(
-                                user=engine_name['user'],
-                                password=engine_name['password'],
-                                account=engine_name['account'],
-                                warehouse=engine_name['warehouse'],
-                                database=engine_name['database'],
-                                schema=engine_name['schema'],
-                                role= engine_name['role'],
-                                numpy=engine_name['numpy']
-                                ))
+    engine = create_engine(URL(user=engine_name['user'],
+                               password=engine_name['password'],
+                               account=engine_name['account'],
+                               warehouse=engine_name['warehouse'],
+                               database=engine_name['database'],
+                               schema=engine_name['schema'],
+                               role=engine_name['role'],
+                               numpy=engine_name['numpy']))
     return engine.connect()
 
 
 def is_file_loaded(process_id, process_name, file, connection, load_type):
-    get_run_id = connection.execute(''' select run_id from etl_process where process_id = {0} 
+    get_run_id = connection.execute(''' select run_id from etl_process where process_id = {0}
                                           and process_name = '{1}' and
-                                          file_name= '{2}' and load_type='{3}' '''.format(process_id,process_name, file, load_type))
+                                          file_name= '{2}' and load_type='{3}' '''.format(process_id,
+                                                                                          process_name,
+                                                                                          file, load_type))
     run_id = get_run_id.fetchall()
     if len(run_id) > 0:
         file_previously_loaded_check = True
@@ -39,7 +38,7 @@ def is_file_loaded(process_id, process_name, file, connection, load_type):
 
 def delete_existing_rows(table_name, run_id, connection):
     print('File was already loaded, deleting records from the table {0}'.format(table_name))
-    ### delete records from table with that run_id
+    # delete records from table with that run_id
     delete_records = connection.execute(''' delete from {0} where run_id = {1}'''.format(
         table_name, run_id))
     deleted_records = delete_records.fetchall()[0][0]
@@ -67,7 +66,7 @@ def transform_file(fname, index1, index2, compareString, ignore_index, ignore_to
 
 def fetch_file_details(file):
     fname = file.split("_")[0] + "_" + file.split("_")[1]
-    fdate = datetime.datetime.strptime(file.split("_")[2].split(".")[0],'%Y%m%d')
+    fdate = datetime.datetime.strptime(file.split("_")[2].split(".")[0], '%Y%m%d')
     fdate = datetime.datetime.strftime(fdate, '%Y-%m-%d')
     ftype = file.split(".")[-1].upper()
     return fname, fdate, ftype
@@ -80,7 +79,7 @@ def create_staged_data(process_id, process_name, filedata, filename):
     staged_data["run_id"] = None
     staged_data["createdate"] = datetime.datetime.now()
     staged_data["file_name"] = filename
-    staged_data = staged_data[['processid', 'custodian_name', 'run_id', "data", "createdate","file_name"]]
+    staged_data = staged_data[['processid', 'custodian_name', 'run_id', "data", "createdate", "file_name"]]
 
     return staged_data
 
@@ -90,14 +89,14 @@ def insert_into_etl_process(connection, *args):
     connection.execute(''' insert into etl_process(
                        PROCESS_ID,PROCESS_NAME,CUSTODIAN_NAME,SRC_FILE_TABLE_NAME,FILE_TYPE,
                        TGT_TABLE,LOAD_TYPE,FILE_NAME,RECORDS_LOADED,STATUS,FILE_DATE,
-                       MODIFIED_DATE,TOTAL_RECORD_COUNT,RECORDS_REJECTED) 
+                       MODIFIED_DATE,TOTAL_RECORD_COUNT,RECORDS_REJECTED)
                        values ({0},'{1}','{1}','{2}','{3}','{4}','{5}','{6}',{7},'{8}','{9}','{10}',{11},{12})'''.format(
                        rec[0], rec[1], rec[2], rec[3], rec[4], rec[5], rec[6], rec[7], rec[8], rec[9], rec[10],
                        rec[11], rec[12]))
 
 
 def fetch_run_id(connection, process_id, process_name, file, load_type):
-    get_newrun_id = connection.execute(''' select run_id from etl_process where process_id = {0} 
+    get_newrun_id = connection.execute(''' select run_id from etl_process where process_id = {0}
                                           and process_name = '{1}' and
                                           file_name= '{2}' and load_type='{3}' '''.format(process_id, process_name, file, load_type))
     newrun_id = get_newrun_id.fetchall()
@@ -111,9 +110,9 @@ def update_run_id(connection, table_name, run_id, file):
 
 def update_etl_process(connection, *args):
     rec = args[0]
-    connection.execute(''' update etl_process set MODIFIED_DATE = '{10}',RECORDS_LOADED = {7}, 
+    connection.execute(''' update etl_process set MODIFIED_DATE = '{10}',RECORDS_LOADED = {7},
     TOTAL_RECORD_COUNT = {11}, TGT_TABLE = '{4}', RECORDS_REJECTED = {12}, STATUS='{8}', FILE_DATE='{9}',
-    FILE_TYPE='{3}', LOAD_TYPE='{5}', SRC_FILE_TABLE_NAME = '{2}' where process_id = {0} 
+    FILE_TYPE='{3}', LOAD_TYPE='{5}', SRC_FILE_TABLE_NAME = '{2}' where process_id = {0}
     and process_name = '{1}' and file_name= '{6}' and run_id = {13} '''.format(rec[0], rec[1], rec[2], rec[3], rec[4],
                                                                                rec[5], rec[6], rec[7], rec[8], rec[9],
                                                                                rec[10], rec[11], rec[12], rec[13]))
@@ -121,8 +120,8 @@ def update_etl_process(connection, *args):
 
 def fetch_col_specifications(pid, pname, fname_pattern, connection):
 
-    col_specs = pd.read_sql_query(''' select col_specs from columnspecification where 
-                                          process_id = {0} and process_name = '{1}' and 
+    col_specs = pd.read_sql_query(''' select col_specs from columnspecification where
+                                          process_id = {0} and process_name = '{1}' and
                                           file_name_pattern= '{2}' '''.format(pid, pname, fname_pattern),
                                   connection)
 
@@ -133,8 +132,8 @@ def fetch_col_specifications(pid, pname, fname_pattern, connection):
 
     result = list(tuple((int(x[0]), int(x[1])) for x in index))
 
-    column_names = pd.read_sql_query(''' select column_names from columnspecification 
-                                             where process_id = {0} and process_name = '{1}' and 
+    column_names = pd.read_sql_query(''' select column_names from columnspecification
+                                             where process_id = {0} and process_name = '{1}' and
                                              file_name_pattern= '{2}' '''.format(pid, pname, fname_pattern),
                                      connection)
     column_names_list = column_names['column_names'][0].split(',')
@@ -148,16 +147,17 @@ def put_and_copy_file(folder_path, data_frame, connection, table_name, stage_nam
 
     # file_size = os.path.getsize(folder_path+ '\Test_CSV_file_to_stage.csv')
     # if file_size > 200000000:  # Check if file size is greater than 200MB
-        # fs = FileSplit(file=folder_path+ '\Test_CSV_file_to_stage.csv', splitsize=50000000, output_dir=folder_path)  # Split each file into 50MB
-        # fs.split()
-        # os.remove(folder_path+ '\Test_CSV_file_to_stage.csv')  # Need to check if we have to delete the file
+    # fs = FileSplit(file=folder_path+ '\Test_CSV_file_to_stage.csv', splitsize=50000000,
+    # output_dir=folder_path)  # Split each file into 50MB
+    # fs.split()
+    # os.remove(folder_path+ '\Test_CSV_file_to_stage.csv')  # Need to check if we have to delete the file
 
-    pd.read_sql_query("put file://" + folder_path + "\\"+ file_name + " @{0}".format(stage_name),
+    pd.read_sql_query("put file://" + folder_path + "\\" + file_name + " @{0}".format(stage_name),
                       connection)
 
     pd.read_sql_query("copy into {0} from @{1}/{2}.gz force = true on_error = 'continue'".format(table_name, stage_name, file_name), connection)
 
-    os.remove(folder_path + "\\"+ file_name)
+    os.remove(folder_path + "\\" + file_name)
 
 
 def stage_to_source(process_id, process_name, file_name_pattern, stage_table, source_table, file,
@@ -165,59 +165,59 @@ def stage_to_source(process_id, process_name, file_name_pattern, stage_table, so
     print("Transferring: ", str(file_name_pattern), "id", process_id, "name", str(process_name))
     run_id = pd.read_sql_query(''' select run_id from etl_process where file_name = '{0}' and load_type = 'FILE TO STG' '''.format(file), conf_connection)
     run_id = run_id['run_id'][0]
-    
-    data_from_stage = pd.read_sql_query(''' select data from {0} where run_id = {1}'''.format(stage_table,run_id), stg_connection)
+
+    data_from_stage = pd.read_sql_query(''' select data from {0} where run_id = {1}'''.format(stage_table,
+                                                                                              run_id), stg_connection)
     print("data_from_stage ----- ", data_from_stage.head())
-    
-    working_folder = pd.read_sql_query('''select local_folder from filelocation where process_id={0}'''
-    								   .format(process_id), conf_connection)['local_folder'][0]
+
+    working_folder = pd.read_sql_query('''select local_folder from filelocation where process_id={0}'''.format(process_id),
+                                       conf_connection)['local_folder'][0]
     working_folder = working_folder.strip('"')
-    
+
     if not os.path.exists(working_folder + '\csv'):
-    	os.mkdir(working_folder + '\csv')
+        os.mkdir(working_folder + '\csv')
     working_folder = working_folder + '\csv'
-    
+
     file_name = 'Test_{0}.csv'.format(os.path.splitext(file)[0])
-    
-    data_from_stage.to_csv(working_folder+'\\'+file_name, sep='`',header=False,index=False, na_rep='',quoting=csv.QUOTE_NONE)
-    data = pd.read_fwf(working_folder+'\\'+file_name, colspecs=col_specs, header=None,
-    				   names=col_names, dtype=str, na_values=' ', keep_default_na=False,encoding='utf-8')
+
+    data_from_stage.to_csv(working_folder + '\\' + file_name, sep='`', header=False, index=False, na_rep='',
+                           quoting=csv.QUOTE_NONE)
+    data = pd.read_fwf(working_folder + '\\' + file_name, colspecs=col_specs, header=None, names=col_names, dtype=str,
+                       na_values=' ', keep_default_na=False, encoding='utf-8')
     data['file_name'] = file
-    
+
     print("data_from_stage ----- ", data.head())
-    os.remove(working_folder + "\\"+ file_name)
-    
+    os.remove(working_folder + "\\" + file_name)
+
     return working_folder, data
-    
-    
+
 
 def fetch_col_specifications_PRSH(process_id, process_name, fname, config_connection):
-
     print(fname)
-    master_col_specs = pd.read_sql_query(''' select col_specs from columnspecification where 
-                                          process_id = {0} and process_name = '{1}' and 
+    master_col_specs = pd.read_sql_query(''' select col_specs from columnspecification where
+                                          process_id = {0} and process_name = '{1}' and
                                           file_name_pattern= '{2}' '''.format(process_id, process_name, fname),
-                                  config_connection)
-    master_column_names = pd.read_sql_query(''' select column_names from columnspecification 
-                                                 where process_id = {0} and process_name = '{1}' and 
-                                                 file_name_pattern= '{2}' '''.format(process_id, process_name, fname),
                                          config_connection)
+    master_column_names = pd.read_sql_query(''' select column_names from columnspecification
+                                                 where process_id = {0} and process_name = '{1}' and
+                                                 file_name_pattern= '{2}' '''.format(process_id, process_name, fname),
+                                            config_connection)
 
     col_specs_list = master_col_specs['col_specs'][0].split('|')
     column_names_list = master_column_names['column_names'][0].split('|')
     final_specs_list = []
     final_column_names_list = []
-    for col_specs, column_names in zip(col_specs_list,column_names_list):
-        
+    for col_specs, column_names in zip(col_specs_list, column_names_list):
+
         header_str = col_specs.split(';')
         index = []
-        
+
         for item in header_str:
             if(len(item) > 0):
                 index.append((item.split(',')[0], item.split(',')[1]))
-    
+
         result = list(tuple((int(x[0]), int(x[1])) for x in index))
-    
+
         result2 = column_names.split(',')
         result2 = [x.upper() for x in result2]
         final_specs_list.append(result)
@@ -225,14 +225,16 @@ def fetch_col_specifications_PRSH(process_id, process_name, fname, config_connec
 
     return final_specs_list, final_column_names_list
 
+
 def stage_to_source_PRSH(process_id, process_name, fname, stg_table_name, table_name, file, column_specs_map,
-                        column_name_map, stage_connection, config_connection):
+                         column_name_map, stage_connection, config_connection):
     print("Transferring: ", str(fname), "id", process_id, "name", str(process_name))
 
     run_id = pd.read_sql_query(''' select run_id from etl_process where file_name = '{0}' and load_type = 'FILE TO STG' '''.format(file), config_connection)
     run_id = run_id['run_id'][0]
 
-    data_from_stage = pd.read_sql_query(''' select data from {0} where run_id = {1}'''.format(stg_table_name,run_id), stage_connection)
+    data_from_stage = pd.read_sql_query(''' select data from {0} where run_id = {1}'''.format(stg_table_name, run_id),
+                                        stage_connection)
     print("data_from_stage ----- ", data_from_stage.head())
 
     working_folder = pd.read_sql_query('''select local_folder from filelocation where process_id={0}'''
@@ -242,36 +244,46 @@ def stage_to_source_PRSH(process_id, process_name, fname, stg_table_name, table_
     if not os.path.exists(working_folder + '\csv'):
         os.mkdir(working_folder + '\csv')
     working_folder = working_folder + '\csv'
-    if (fname == 'PRSH_ACCT' or fname == 'PRSH_ACCF'):
-        recordTypes = ['A','B', 'C', 'D', 'E', 'W','F']
-    elif (fname == 'PRSH_GTDE' or fname == 'PRSH_GSDE'):
-        recordTypes = ['A','B']
-    elif (fname == 'PRSH_ISCA'):
+    if fname == 'PRSH_ACCT' or fname == 'PRSH_ACCF':
+        recordTypes = ['A', 'B', 'C', 'D', 'E', 'W', 'F']
+    elif fname == 'PRSH_GTDE' or fname == 'PRSH_GSDE':
+        recordTypes = ['A', 'B']
+    elif fname == 'PRSH_ISCA':
         recordTypes = ['A', 'C', 'D', 'E', 'F', 'G']
-            
+
     column_master_list = []
     for recordType in recordTypes:
         for x in column_name_map[recordType]:
-            if(len(x) > 0 ):
+            if len(x) > 0:
                 column_master_list.append(x.strip())
-            
+
     column_master_list = list(dict.fromkeys(column_master_list))
     data = pd.DataFrame(columns=column_master_list)
     file_name = 'Test_{0}.csv'.format(file.split('.')[0])
     for recordType in recordTypes:
         print(recordType)
-        if (fname == 'PRSH_ISCA'):
-            data_from_stage[data_from_stage['data'].apply(lambda x : x[0:1] == recordType)].to_csv(working_folder+'\\'+file_name, sep='`',header=False,index=False, na_rep='',quoting=csv.QUOTE_NONE)
+        if fname == 'PRSH_ISCA':
+            data_from_stage[data_from_stage['data'].apply(lambda x: x[0:1] == recordType)].to_csv(working_folder + '\\' + file_name,
+                                                                                                  sep='`',
+                                                                                                  header=False,
+                                                                                                  index=False, na_rep='',
+                                                                                                  quoting=csv.QUOTE_NONE)
         else:
-            data_from_stage[data_from_stage['data'].apply(lambda x : x[2:3] == recordType)].to_csv(working_folder+'\\'+file_name, sep='`',header=False,index=False, na_rep='',quoting=csv.QUOTE_NONE)
-        data_tmp = pd.read_fwf(working_folder+'\\'+file_name, colspecs=column_specs_map[recordType], header=None,
-                       names=column_name_map[recordType], dtype=str, na_values=' ', keep_default_na=False,encoding='utf-8')
-        data = data.append(data_tmp, ignore_index = True, sort=False)
+            data_from_stage[data_from_stage['data'].apply(lambda x: x[2:3] == recordType)].to_csv(working_folder + '\\' + file_name,
+                                                                                                  sep='`',
+                                                                                                  header=False,
+                                                                                                  index=False,
+                                                                                                  na_rep='',
+                                                                                                  quoting=csv.QUOTE_NONE)
+
+        data_tmp = pd.read_fwf(working_folder + '\\' + file_name, colspecs=column_specs_map[recordType], header=None,
+                               names=column_name_map[recordType], dtype=str, na_values=' ', keep_default_na=False,
+                               encoding='utf-8')
+        data = data.append(data_tmp, ignore_index=True, sort=False)
         print(data.shape)
     data['file_name'] = file
-     	        
 
     print("data_from_stage ----- ", data.head())
-    os.remove(working_folder + "\\"+ file_name)
+    os.remove(working_folder + "\\" + file_name)
 
     return working_folder, data
